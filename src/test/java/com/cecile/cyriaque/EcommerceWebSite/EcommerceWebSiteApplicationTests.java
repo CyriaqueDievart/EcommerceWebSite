d3ec995613154ab52e619e5283224f14510eabe8
Cyriaque <cyriaque.dievart@gmail.com>
2020-11-02 14:46:45 +0100

    initial commit

---
+++ src/test/java/com/cecile/cyriaque/EcommerceWebSite/EcommerceWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cecile.cyriaque.EcommerceWebSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
